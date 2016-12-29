74110ff96510c0c3215f7d654608bd761082edc1
Gankuro <nateat9@gmail.com>
2016-12-29 13:08:53 -0800

    Add files via upload

---
+++ colorpalette1.js
@@ -0,0 +1,13 @@
+javascript:(function(){
+//text colors guide:
+/*
+	0 - timer reset
+	1 - other text / timer during
+	2 -  timer fail
+	3 - successful
+*/
+//Carykh minesweeper classic skin - Gankuro(TheRedArmy)
+bgColor = "#bfbebb";
+buttonColor = "9E9E9E";
+	
+})();
