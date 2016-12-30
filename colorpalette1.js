587b9d8901df19f63ab15024cdf8fa05a2cfb42c
Gankuro <nateat9@gmail.com>
2016-12-30 07:44:55 -0800

    Update colorpalette1.js

--- colorpalette1.js
+++ colorpalette1.js
@@ -8,17 +8,23 @@ javascript:(function(){
 */
 //Carykh minesweeper classic skin - Gankuro(TheRedArmy)
 window.open("https://cdn.rawgit.com/Gankuro/mpalettes/master/test.html");
-var colors = prompt("Paste code for palette here")
+var colors; //= prompt("Paste code for palette here")
+//classic palette code: bfbebbA0A0A0000000000000C0000000A0009E9E9E#FF00000040FF008000FF0000000080800080008080606060000000
 bgColor = "#bfbebb";
+tileColor = "#A0A0A0";
+textColors = ["#000000","#000000","#C00000","#00A000"];
 buttonColor = "#9E9E9E";
-textColors[0] = "#000000";
-	
+deathColor = "#FF0000";
+numberColors = [tileColor, "#0040FF","#008000","#FF0000","#000080","#800080","#008080","#606060","#000000"];
+
+/*
+Basic colors
 bgColor = "#AADCFF";
 tileColor = "#A0A0A0";
 textColors = ["#8094A8","#000000","#C00000","#00A000"];
 buttonColor = "#50A0B4";
 deathColor = "#FF0000";
 numberColors = [tileColor, "#0040FF","#008000","#FF0000","#000080","#800080","#008080","#606060","#000000"];
-
+*/
 	
 })();
