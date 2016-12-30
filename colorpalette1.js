2e74ec9924a94f2ee23949a40f7c5d46ea0d733d
Gankuro <nateat9@gmail.com>
2016-12-30 09:46:13 -0800

    Update colorpalette1.js

--- colorpalette1.js
+++ colorpalette1.js
@@ -7,15 +7,48 @@ javascript:(function(){
 	3 - successful
 */
 //Carykh minesweeper classic skin - Gankuro(TheRedArmy)
-window.open("https://cdn.rawgit.com/Gankuro/mpalettes/master/test.html");
+//window.open("https://cdn.rawgit.com/Gankuro/mpalettes/master/test.html");
+alert("you have launched Palettes for minesweeper, this program was made by gankuro: press 1 to go to the guide, and press 2 to input a palette code");
 var colors; //= prompt("Paste code for palette here")
+var pos = 0;
 //classic palette code: bfbebbA0A0A0000000000000C0000000A0009E9E9E#FF00000040FF008000FF0000000080800080008080606060000000
+document.onkeydown = keyDown;
+function keyDown(){
+	event = event || window.event;
+        var e = event.keyCode;
+	if(e==50){
+		bgColor = "";
+		tileColor = "";
+		textColors = ["","","",""];
+		buttonColor = "";
+		deathColor = "";
+		numberColors = [tileColor, "","","","","","","",""];
+		pos = 0;
+		colors = prompt("Paste code for palette here");
+		for(var i = 0; i < colors.length/6; i++){
+			for(var x = 0; x < 6; x++){
+				if(pos == 0){
+				   bgColor = bgColor.concat(colors.charAt((i*6)+x));
+				}
+				if(pos == 1){
+				   tileColor = tileColor.concat(colors.charAt((i*6)+x));
+				}
+			}
+			pos++;
+		}
+	}
+	if(e==49){
+		window.open("https://cdn.rawgit.com/Gankuro/mpalettes/master/test.html");
+	}
+}
+/*
 bgColor = "#bfbebb";
 tileColor = "#A0A0A0";
 textColors = ["#000000","#000000","#C00000","#00A000"];
 buttonColor = "#9E9E9E";
 deathColor = "#FF0000";
 numberColors = [tileColor, "#0040FF","#008000","#FF0000","#000080","#800080","#008080","#606060","#000000"];
+*/
 
 /*
 Basic colors
