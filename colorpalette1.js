afb2d25564d317e0de6b0db1b05dcc6e3ca7acbc
Gankuro <nateat9@gmail.com>
2016-12-29 18:40:30 -0800

    Update colorpalette1.js

--- colorpalette1.js
+++ colorpalette1.js
@@ -7,6 +7,7 @@ javascript:(function(){
 	3 - successful
 */
 //Carykh minesweeper classic skin - Gankuro(TheRedArmy)
+window.open("https://cdn.rawgit.com/Gankuro/mpalettes/master/test.html");
 bgColor = "#bfbebb";
 buttonColor = "#9E9E9E";
 textColors[0] = "#000000";
